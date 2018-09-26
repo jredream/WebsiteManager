2052c254509ab06f29bc1dcf1c8632538de8d5e0
zhangbz <zhbzh1985@gmail.com>
2018-09-26 10:33:47 +0800

    Update ws-main.js

--- js/ws-main.js
+++ js/ws-main.js
@@ -32,73 +32,29 @@ var app = new Vue({
     initData() {
       // 初始化分组
       DBService.createGroup({
-        title: "正式环境",
+        title: "工具箱",
         listType: "list"
       }).then(id => {
         console.log("id=", id);
         let arr = [
           {
-            title: "Boss后台",
-            url: "https://admin.hh-medic.com/admin/index",
+            title: "Google",
+            url: "https://www.google.com",
             groupId: id
           },
           {
-            title: "家庭医生",
-            url: "https://fadmin.hh-medic.com/dashboard",
+            title: "Baidu",
+            url: "https://www.baidu.com",
             groupId: id
-          },
-          { title: "和缓天地", url: "http://bd.hh-medic.com", groupId: id }
+          }
         ];
-        console.log(arr);
         for (var i = 0; i < arr.length; i++) {
           var record = arr[i];
           console.log(record);
           DBService.createWebSite(record);
         }
-      });
-
-      DBService.createGroup({
-        title: "测试环境",
-        listType: "list"
-      }).then(id => {
-        let arr = [
-          {
-            title: "Boss后台",
-            url: "http://test.hh-medic.com/admin",
-            groupId: id
-          },
-          {
-            title: "家庭医生",
-            url: "https://fadmin-test-temp.hh-medic.com/dashboard",
-            groupId: id
-          },
-          { title: "和缓天地", url: "http://test.hh-medic.com/bd", groupId: id }
-        ];
-        for (var i = 0; i < arr.length; i++) {
-          var record = arr[i];
-          DBService.createWebSite(record);
-        }
-      });
-
-      DBService.createGroup({
-        title: "研发后台",
-        listType: "button"
-      }).then(id => {
-        let arr = [
-          { title: "CI", url: "https://ci.hh-medic.com/", groupId: id },
-          { title: "Code", url: "https://code.hh-medic.com", groupId: id },
-          { title: "API", url: "http://api.hh-medic.com", groupId: id },
-          { title: "Log", url: "https://log.hh-medic.com", groupId: id },
-          { title: "Docker", url: "http://docker.hh-medic.com", groupId: id }
-        ];
-        for (var i = 0; i < arr.length; i++) {
-          var record = arr[i];
-          DBService.createWebSite(record);
-        }
-        console.log(EventBus);
-        console.log("refreshList - emit");
-        EventBus.$emit("refreshList");
-      });
+      });     
+      
     }
   },
   watch: {
